ab5c5a7adf7bb18311182fc06fbda6a10ad9daae
Akhil <pakhilnair@gmail.com>
2024-10-09 15:46:10 +0530

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import "./App.css";
 import UserLayout from "./layout/UserLayout";
-import NavBar from "./components/NavBar";
 import Dashboard from "./pages/Dashboard";
+import { Route, Routes, Link, BrowserRouter } from "react-router-dom";
+import Learning from "./pages/Learning";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
+  // return <RouterProvider router={router} />;
+  // <>
+  //   <NavBar />
+  //   <div className="container flex p-4 mx-auto md:space-x-4">
+  //     <UserLayout />
+  //     <Dashboard />
+  //   </div>
+  // </>
+
   return (
-    <>
-      <NavBar />
-      <div className="container flex p-4 mx-auto md:space-x-4">
-        <UserLayout />
-        <Dashboard />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/learning" element={<Learning />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -6,23 +6,31 @@ import {
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import SideBarItem from "../components/SideBarItem";
+import NavBar from "../components/NavBar";
+import { Outlet } from "react-router-dom";
 
 const userLayout = () => {
   const sidebarItems = [
-    { name: "Dashboard", icon: faGauge, route: "/student/dashboard" },
-    { name: "Learning", icon: faBookOpen, route: "/student/learning" },
-    { name: "Health", icon: faHeartCircleCheck, route: "/student/health" },
-    { name: "Mind", icon: faBrain, route: "/student/mind" },
-    { name: "Profile", icon: faUser, route: "/student/profile" },
+    { name: "Dashboard", icon: faGauge, route: "/dashboard" },
+    { name: "Learning", icon: faBookOpen, route: "/learning" },
+    { name: "Health", icon: faHeartCircleCheck, route: "/health" },
+    { name: "Mind", icon: faBrain, route: "/mind" },
+    { name: "Profile", icon: faUser, route: "/profile" },
   ];
   return (
-    <div className="sidebar h-screen hidden md:block">
-      <div className="flex-col justify-evenly">
-        {sidebarItems.map((item, index) => (
-          <SideBarItem item={item.name} icon={item.icon} key={index} />
-        ))}
+    <>
+      <NavBar />
+      <div className="container flex p-4 mx-auto md:space-x-4">
+        <div className="sidebar h-screen hidden md:block">
+          <div className="flex-col justify-evenly">
+            {sidebarItems.map((item, index) => (
+              <SideBarItem item={item.name} icon={item.icon} key={index} />
+            ))}
+          </div>
+        </div>
+        <Outlet />
       </div>
-    </div>
+    </>
   );
 };
 
