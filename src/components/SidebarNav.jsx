e67c543c320b5fc3adc65d32174ec8beda21a2c5
Akhil <pakhilnair@gmail.com>
2024-10-11 22:45:16 +0530

    Typeerror fixed in console

--- src/components/SidebarNav.jsx
+++ src/components/SidebarNav.jsx
@@ -3,24 +3,20 @@ import { NavLink } from "react-router-dom";
 import sidebarItems from "../data/sidebarItems";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const SidebarNav = (toggleSidebar) => {
+const SidebarNav = ({ toggleSidebar }) => {
   return (
     <div
-      className="flex flex-col py-10 pr-2 fixed top-0 left-0 h-full w-72 z-10 md:w-96 bg-gray-900 text-white transform 
+      className="flex flex-col py-10 fixed top-0 left-0 h-full w-72 z-10 md:w-96 bg-gray-900 text-white transform 
                 translate-x-0 transition-transform duration-300 ease-in-out overflow-y-auto"
     >
       <NavLink to="/">
-        <img
-          src="../../public/logo.png"
-          alt="Brainloop logo"
-          className="max-w-32 ml-10"
-        />
+        <img src="/logo.png" alt="Brainloop logo" className="max-w-32 ml-10" />
       </NavLink>
-      <div className="mt-10">
+      <div className="border-t border-gray-700 mt-6 pb-1" />
+      <div className="mt-6 mr-2">
         {sidebarItems.map((item, index) => (
-          <div onClick={() => toggleSidebar()}>
+          <div onClick={() => toggleSidebar()} key={index}>
             <NavLink
-              key={index}
               to={item.route}
               className={({ isActive }) =>
                 `flex items-center py-4 pl-10 my-2 hover:bg-gray-800 hover:cursor-pointer rounded-e-lg
