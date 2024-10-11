2927db57f9268691173693ecf86f7836879f597b
Akhil <pakhilnair@gmail.com>
2024-10-11 19:18:10 +0530

    Logo change. Navbar cleanup

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,44 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faChevronDown,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="container p-6 mx-auto">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center">
-          <p className="text-3xl pr-2">
-            <FontAwesomeIcon icon={faBars} />
-          </p>
-          <div className="flex-col">
-            <p className="text-xs text-blue-600">BRAINLOOP</p>
-            <p className="text-sm md:text-1xl">Deep identification</p>
-          </div>
+      <div className="flex items-center justify-between md:justify-items-center">
+        <div className="md:hidden pr-2">
+          <FontAwesomeIcon icon={faBars} size="xl" />
         </div>
-        <div className="flex">
-          <p className="text-xs md:text-sm">Raman Kartha</p>
+        <NavLink to="/">
+          {/* <div className="flex items-center">
+            <p className="md:hidden text-3xl pr-2">
+              <FontAwesomeIcon icon={faBars} />
+            </p>
+            <div className="flex-col">
+              <p className="text-xs text-blue-600">BRAINLOOP</p>
+              <p className="text-sm md:text-1xl">Deep identification</p>
+            </div>
+          </div> */}
+
+          <img
+            src="../../public/logo.png"
+            alt="Brainloop logo"
+            className="max-w-28 md:max-w-36"
+          />
+        </NavLink>
+
+        <div className="hidden md:flex items-center">
+          <p className="text-sm">Raman Kartha</p>
           <p className="pl-1">
             <FontAwesomeIcon icon={faChevronDown} size="sm" />
           </p>
+        </div>
+        <div className="md:hidden">
+          <FontAwesomeIcon icon={faUser} size="lg" />
         </div>
       </div>
     </nav>
